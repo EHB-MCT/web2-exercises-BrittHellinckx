4258a8aab43654fe9c640321bd885a0708a45efe
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-12-05 16:36:42 +0100

    doing ex2 with tutorial (part1)

--- WC6/ex2/index.js
+++ WC6/ex2/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const fs = require('fs/promises')
 const app = express()
 const port = 3000
 
@@ -7,26 +8,71 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 
+//root route
 app.get('/', (req, res) => {
     res.status(300).redirect('/info.html');
 })
 
-app.get('/api/boardgames', (req, res) => {
-    let exampleData = {
-        name: 'britt',
-        age: 19,
-        cats: ['twaila', 'lowie']
+//return all boardgames
+app.get('/boardgames', async (req, res) => {
+    try {
+        //read file
+        let data = await fs.readFile('data/boardgames.json')
+        //send back file
+        res.status(200).send(JSON.parse(data));
+    } catch (error) {
+        res.status(500).send('file could not be read');
     }
-    res.send(exampleData);
+
+})
+//return one boardgame
+app.get('/boardgame', async (req, res) => {
+    try {
+        //read file
+        let boardgames = await fs.readFile('data/boardgames.json');
+        boardgames = JSON.parse(boardgames);
+        // try and find board game with id
+        let bg = boardgames[req.query.id];
+        if (bg) {
+            //send back file
+            res.status(200).send(bg);
+            return;
+        } else {
+            res.status(400).send('boardgame could not be found with id:' + req.query.id);
+        }
+
+    } catch (error) {
+        res.status(500).send('file could not be read');
+    }
 })
 
-app.get('/text', (req, res) => {
-    res.send('Hello, hope you have a nice day!');
-})
+//save boardgame
+app.post('/saveBoardgame', async (req, res) => {
+    if (!req.body.id || !req.body.name || !req.body.genre || !req.body.mechanisms || !req.body.description) {
+        res.status(400).send('bad result, missing id, name, genre, mechanisms or description');
+        return;
+    }
+    try {
+        //read file
+        let boardgames = await fs.readFile('data/boardgames.json');
+        boardgames = JSON.parse(boardgames);
 
-app.post('/saveData',(req, res) => {
-    console.log(req.body);
-    res.send(`Data recieved with id: ${req.body.id}`);
+        //save the boardgame
+        boardgames[req.body.id] = {
+            name: req.body.name,
+            genre: req.body.genre,
+            mechanisms: req.body.mechanisms,
+            description: req.body.description
+        }
+        //save the file
+        await fs.writeFile('data/boardgames.json', JSON.stringify(boardgames));
+        //send back succes message
+        res.status(201).send('boardgame successfully saved with id:' + req.body.id);
+        return;
+    } catch (error) {
+        res.status(500).send('an error has occured');
+    }
+
 })
 
 app.listen(port, () => {
